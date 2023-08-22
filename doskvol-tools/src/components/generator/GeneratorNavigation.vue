880a16cf6804c5796619df40756dd63977b554a6
Katarzyna <kasiakadziolka@gmail.com>
2023-08-22 12:38:28 +0200

    Maybe path fix

---
+++ doskvol-tools/src/components/generator/GeneratorNavigation.vue
@@ -0,0 +1,27 @@
+<script lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    <div>
+      Ulice
+    </div>
+    <div>
+      Ulice
+    </div>
+    <div>
+      Ulice
+    </div>
+    <div>
+      Ulice
+    </div>
+    <div>
+      Ulice
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+
+</style>
--- doskvol-tools/src/views/GeneratorView.vue
+++ doskvol-tools/src/views/GeneratorView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import GeneratorNavigation from '@/components/generator/GeneratorNavigation.vue'
 
 </script>
 
 <template>
-  <div>Generator!</div>
+  <div>
+    <GeneratorNavigation />
+    Generator!
+  </div>
 </template>
