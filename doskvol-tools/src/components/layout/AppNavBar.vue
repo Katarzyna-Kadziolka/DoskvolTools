65c539739cb85b6e920d903a01093b9f5995972e
Katarzyna <kasiakadziolka@gmail.com>
2023-08-22 11:42:32 +0200

    Add font

---
+++ doskvol-tools/src/components/layout/AppNavBar.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class='app-nav-bar_container'>Doskvol Tools</div>
+</template>
+
+<style scoped lang='scss'>
+.app-nav-bar {
+  &_container {
+    background: $color-secondary;
+    height: 6vh;
+  }
+}
+</style>
--- doskvol-tools/src/main.ts
+++ doskvol-tools/src/main.ts
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import "@fontsource/source-sans-pro"; // Defaults to weight 400
+import "@fontsource/source-sans-pro/400.css"; // Specify weight
+import "@fontsource/source-sans-pro/400-italic.css"; // Specify weight and style
 
 const app = createApp(App)
 
