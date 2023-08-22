29d4d97db4da4e4a7624c6284f49d40dc581f035
Katarzyna <kasiakadziolka@gmail.com>
2023-08-22 10:53:05 +0200

    Fix structure

--- doskvol-tools/src/views/AboutView.vue
+++ doskvol-tools/src/views/AboutView.vue
@@ -4,12 +4,17 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
+  h1 {
+    color: $color-primary;
+  }
 }
 </style>
+<script setup lang="ts">
+</script>
