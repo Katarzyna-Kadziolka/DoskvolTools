522eff50cae9162010a5ebf8291813673947cdba
Katarzyna <kasiakadziolka@gmail.com>
2023-08-22 11:06:32 +0200

    path fix

--- doskvol-tools/src/router/index.ts
+++ doskvol-tools/src/router/index.ts
@@ -1,22 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import GeneratorView from '../views/GeneratorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'generatorView',
+      component: GeneratorView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
