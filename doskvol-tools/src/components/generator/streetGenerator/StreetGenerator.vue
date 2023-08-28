95f1f5a6e0fdcd9a88a4b126aa57cb72cbefd23b
Katarzyna <kasiakadziolka@gmail.com>
2023-08-28 12:41:13 +0200

    BaseButton

--- doskvol-tools/src/components/generator/streetGenerator/StreetGenerator.vue
+++ doskvol-tools/src/components/generator/streetGenerator/StreetGenerator.vue
@@ -1,6 +1,7 @@
 <script setup lang='ts'>
 import BaseRadioButtons from '../../common/BaseRadioButtons.vue'
 import {ref} from 'vue'
+import BaseButton from "@/components/common/BaseButton.vue";
 
 const commonness = ["Pospolity", "Rzadki"]
 const changes = ref("")
@@ -12,8 +13,17 @@ const onPickedChanges = (pickedValues: string) => {
 </script>
 
 <template>
-  <BaseRadioButtons  name="commonness" :values=commonness  @pickedChanged="onPickedChanges"/>
-  {{changes}}
+  <div class="street-generator">
+    <BaseRadioButtons  name="commonness" :values=commonness  @pickedChanged="onPickedChanges"/>
+    {{changes}}
+    <BaseButton name="Wygeneruj" />
+    
+  </div>
+
 </template>
 
-<style scoped lang='scss'></style>
+<style scoped lang='scss'>
+.street-generator {
+  margin-top: 50px;
+}
+</style>
