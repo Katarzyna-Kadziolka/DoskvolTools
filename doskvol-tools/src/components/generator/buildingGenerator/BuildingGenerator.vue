c234120872c40f67ad3b48c215de0522400c4cc1
Katarzyna <kasiakadziolka@gmail.com>
2023-09-04 13:06:02 +0200

    Extract base generator

--- doskvol-tools/src/components/generator/buildingGenerator/BuildingGenerator.vue
+++ doskvol-tools/src/components/generator/buildingGenerator/BuildingGenerator.vue
@@ -6,9 +6,8 @@ import useRandom from "@/composables/Random";
 import {useBuildingStore} from "@/stores/BuildingStore";
 import type {TableVariant, Variant} from "@/types/TableVariant";
 import BaseTable from "@/components/common/baseTable/BaseTable.vue";
-import BaseCard from "@/components/common/BaseCard.vue";
 import GeneratorResultElement from "@/components/generator/GeneratorResultElement.vue";
-import BaseButton from "@/components/common/BaseButton.vue";
+import BaseGenerator from "@/components/generator/BaseGenerator.vue";
 
 const {t} = useI18n({useScope: "global"});
 
@@ -49,59 +48,21 @@ const generateRandomVariant = () => {
 </script>
 
 <template>
-  <div class="building-generator_container">
-    <div>
+  <BaseGenerator :title="t('generator-navigation.building').toLocaleUpperCase()" @generateRandom="generateRandom">
+    <template v-slot:tables>
       <BaseTable v-model="building.exteriorMaterial" :table-data="buildingData.getExteriorMaterial()"/>
       <BaseTable v-model="building.exteriorDetails" :table-data="buildingData.getExteriorDetails()"/>
       <BaseTable v-model="building.use" :table-data="buildingData.getCommonUse()"/>
       <BaseTable v-model="building.use" :table-data="buildingData.getRareUse()"/>
       <BaseTable v-model="building.details" :table-data="buildingData.getDetails()"/>
       <BaseTable v-model="building.items" :table-data="buildingData.getItems()"/>
-    </div>
-    <BaseCard class="building-generator_base-card">
-      <div class="building-generator_base-card-result">
-        <span class="building-generator_title">{{t("generator-navigation.building").toLocaleUpperCase()}}</span>
-        <GeneratorResultElement :title="t('buildings.exterior.material.title')" :value="building.exteriorMaterial"/>
-        <GeneratorResultElement :title="t('buildings.exterior.details.title')" :value="building.exteriorDetails"/>
-        <GeneratorResultElement :title="t('buildings.use.title')" :value="building.use"/>
-        <GeneratorResultElement :title="t('buildings.details.title')" :value="building.details"/>
-        <GeneratorResultElement :title="t('buildings.items.title')" :value="building.items"/>
-      </div>
-      <BaseButton :name="t('generator-ui.generate')" @click="generateRandom"/>
-    </BaseCard>
-  </div>
+    </template>
+    <template v-slot:results>
+      <GeneratorResultElement :title="t('buildings.exterior.material.title')" :value="building.exteriorMaterial"/>
+      <GeneratorResultElement :title="t('buildings.exterior.details.title')" :value="building.exteriorDetails"/>
+      <GeneratorResultElement :title="t('buildings.use.title')" :value="building.use"/>
+      <GeneratorResultElement :title="t('buildings.details.title')" :value="building.details"/>
+      <GeneratorResultElement :title="t('buildings.items.title')" :value="building.items"/>
+    </template>
+  </BaseGenerator>
 </template>
-
-<style scoped lang='scss'>
-.building-generator {
-  &_container {
-    margin-top: 40px;
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    width: 100%;
-  }
-
-  &_base-card {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    min-height: 50vh;
-    width: 20vw;
-    position: fixed;
-    right: 32px;
-    top: 128px;
-  }
-
-  &_title {
-    font-size: 1.2rem;
-    text-align: center;
-    margin-bottom: 8px;
-  }
-  &_base-card-result {
-    display: flex;
-    flex-direction: column;
-    row-gap: 8px;
-    margin-bottom: 24px;
-  }
-}
-</style>
--- doskvol-tools/src/components/generator/streetGenerator/StreetGenerator.vue
+++ doskvol-tools/src/components/generator/streetGenerator/StreetGenerator.vue
@@ -1,13 +1,12 @@
 <script setup lang='ts'>
-import BaseButton from "@/components/common/BaseButton.vue";
 import BaseTable from "@/components/common/baseTable/BaseTable.vue";
-import BaseCard from "@/components/common/BaseCard.vue";
 import {useStreetStore} from "@/stores/StreetStore";
 import {storeToRefs} from "pinia";
 import {useI18n} from 'vue-i18n'
 import {useStreetGenerator} from "@/components/generator/streetGenerator/Composables/StreetGeneratorContent";
 import GeneratorResultElement from "@/components/generator/GeneratorResultElement.vue";
 import useRandom from "@/composables/Random";
+import BaseGenerator from "@/components/generator/BaseGenerator.vue";
 
 const {t} = useI18n({useScope: "global"});
 
@@ -31,8 +30,8 @@ const generateRandom = () => {
 </script>
 
 <template>
-  <div class="street-generator_container">
-    <div>
+  <BaseGenerator :title="t('generator-navigation.street').toLocaleUpperCase()" @generateRandom="generateRandom">
+    <template v-slot:tables>
       <BaseTable v-model="street.mood" :table-data="streetData.getMood()"/>
       <BaseTable v-model="street.impressionsSights" :table-data="streetData.getImpressionsSights()"/>
       <BaseTable v-model="street.impressionsSounds" :table-data="streetData.getImpressionsSounds()"/>
@@ -41,54 +40,16 @@ const generateRandom = () => {
       <BaseTable v-model="street.type" :table-data="streetData.getType()"/>
       <BaseTable v-model="street.details" :table-data="streetData.getDetails()"/>
       <BaseTable v-model="street.props" :table-data="streetData.getProps()"/>
-    </div>
-    <BaseCard class="street-generator_base-card">
-      <div class="street-generator_base-card-result">
-        <span class="street-generator_title">{{t("generator-navigation.street").toLocaleUpperCase()}}</span>
-        <GeneratorResultElement :title="t('streets.mood.title')" :value="street.mood"/>
-        <GeneratorResultElement :title="t('streets.impressions.sights.title')" :value="street.impressionsSights"/>
-        <GeneratorResultElement :title="t('streets.impressions.sounds.title')" :value="street.impressionsSounds"/>
-        <GeneratorResultElement :title="t('streets.impressions.smells.title')" :value="street.impressionsSmells"/>
-        <GeneratorResultElement :title="t('streets.use.title')" :value="street.use"/>
-        <GeneratorResultElement :title="t('streets.type.title')" :value="street.type"/>
-        <GeneratorResultElement :title="t('streets.details.title')" :value="street.details"/>
-        <GeneratorResultElement :title="t('streets.props.title')" :value="street.props"/>
-      </div>
-      <BaseButton :name="t('generator-ui.generate')" @click="generateRandom"/>
-    </BaseCard>
-  </div>
+    </template>
+    <template v-slot:results>
+      <GeneratorResultElement :title="t('streets.mood.title')" :value="street.mood"/>
+      <GeneratorResultElement :title="t('streets.impressions.sights.title')" :value="street.impressionsSights"/>
+      <GeneratorResultElement :title="t('streets.impressions.sounds.title')" :value="street.impressionsSounds"/>
+      <GeneratorResultElement :title="t('streets.impressions.smells.title')" :value="street.impressionsSmells"/>
+      <GeneratorResultElement :title="t('streets.use.title')" :value="street.use"/>
+      <GeneratorResultElement :title="t('streets.type.title')" :value="street.type"/>
+      <GeneratorResultElement :title="t('streets.details.title')" :value="street.details"/>
+      <GeneratorResultElement :title="t('streets.props.title')" :value="street.props"/>
+    </template>
+  </BaseGenerator>
 </template>
-
-<style scoped lang='scss'>
-.street-generator {
-  &_container {
-    margin-top: 40px;
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    width: 100%;
-  }
-
-  &_base-card {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    min-height: 50vh;
-    width: 20vw;
-    position: fixed;
-    right: 32px;
-    top: 128px;
-  }
-
-  &_title {
-    font-size: 1.2rem;
-    text-align: center;
-    margin-bottom: 8px;
-  }
-  &_base-card-result {
-    display: flex;
-    flex-direction: column;
-    row-gap: 8px;
-    margin-bottom: 24px;
-  }
-}
-</style>
