c15ded5038fe4886598108ad1859d7b9fc6168d6
Katarzyna <kasiakadziolka@gmail.com>
2023-09-11 12:18:41 +0200

    bg for page

--- doskvol-tools/src/views/NotFoundView.vue
+++ doskvol-tools/src/views/NotFoundView.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
+import {useI18n} from 'vue-i18n'
+import BaseButton from "@/components/common/BaseButton.vue";
 
+const {t} = useI18n({useScope: "global"});
 </script>
 
 <template>
-  Nothing here
+  <div class="not-found-view_container">
+      <span>{{t('notFound.title')}}</span>
+      <BaseButton :name="t('notFound.content')" />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.not-found-view {
+  &_container {
+    background: url(../assets/blades.jpg) ;
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+  }
+}
+</style>
