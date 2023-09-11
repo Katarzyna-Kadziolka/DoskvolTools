cede6d8c2c20fd94d8cabee5422d118625578beb
Katarzyna <kasiakadziolka@gmail.com>
2023-09-11 12:50:22 +0200

    404 page finish

--- doskvol-tools/src/views/NotFoundView.vue
+++ doskvol-tools/src/views/NotFoundView.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import {useI18n} from 'vue-i18n'
 import BaseButton from "@/components/common/BaseButton.vue";
+import {useRouter} from "vue-router";
 
 const {t} = useI18n({useScope: "global"});
+const router = useRouter();
 </script>
 
 <template>
   <div class="not-found-view_container">
-      <span>{{t('notFound.title')}}</span>
-      <BaseButton :name="t('notFound.content')" />
+    <div class="not-found-view_content-container">
+      <span class="not-found-view_title">{{t('notFound.title')}}</span>
+      <BaseButton :name="t('notFound.content')" @click="router.push({name: 'streetGenerator'})"/>
+    </div>
   </div>
 </template>
 
@@ -20,6 +24,19 @@ const {t} = useI18n({useScope: "global"});
     height: 100%;
     width: 100%;
     background-size: cover;
+  }
+  &_content-container {
+    display: flex;
+    flex-direction: column;
+    margin-left: 20vw;
+    justify-content: center;
+    height: 100%;
+    align-content: center;
+    width: 25vw;
+    row-gap: 2rem;
+  }
+  &_title {
+    font-size: 1.8rem;
   }
 }
 </style>
