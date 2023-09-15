1d5b62339899daeba1c2764b8fbcf555bda29f2d
Katarzyna <kasiakadziolka@gmail.com>
2023-09-15 11:08:08 +0200

    solution

--- doskvol-tools/src/components/generator/GeneratorResultElement.vue
+++ doskvol-tools/src/components/generator/GeneratorResultElement.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
-defineProps<{
+import {computed} from "vue";
+
+const props = defineProps<{
   title: string,
-  value: string
+  value: string,
+  url: string,
 }>()
+
+const fullUrl = computed(() => {
+  return "#" + props.url;
+})
 </script>
 
 <template>
   <div class="generator-result-element_container">
-    <div><b>{{ title }}:</b></div>
+    <a :href="fullUrl"><div><b>{{ title }}:</b></div></a>
     <div>{{ value }}</div>
   </div>
 </template>
 
 <style scoped lang="scss">
+a {
+  color: inherit;
+  text-decoration: none;
+}
 .generator-result-element {
   &_container {
     display: grid;
--- doskvol-tools/src/components/generator/buildingGenerator/BuildingGenerator.vue
+++ doskvol-tools/src/components/generator/buildingGenerator/BuildingGenerator.vue
@@ -50,19 +50,19 @@ const generateRandomVariant = () => {
 <template>
   <BaseGenerator :title="t('generator-navigation.building').toLocaleUpperCase()" @generateRandom="generateRandom">
     <template v-slot:tables>
-      <BaseTable v-model="building.exteriorMaterial" :table-data="buildingData.getExteriorMaterial()"/>
-      <BaseTable v-model="building.exteriorDetails" :table-data="buildingData.getExteriorDetails()"/>
-      <BaseTable v-model="building.use" :table-data="buildingData.getCommonUse()"/>
-      <BaseTable v-model="building.use" :table-data="buildingData.getRareUse()"/>
-      <BaseTable v-model="building.details" :table-data="buildingData.getDetails()"/>
-      <BaseTable v-model="building.items" :table-data="buildingData.getItems()"/>
+      <BaseTable v-model="building.exteriorMaterial" :table-data="buildingData.getExteriorMaterial()" id="buildingExteriorMaterial"/>
+      <BaseTable v-model="building.exteriorDetails" :table-data="buildingData.getExteriorDetails()" id="buildingExteriorDetails"/>
+      <BaseTable v-model="building.use" :table-data="buildingData.getCommonUse()" id="buildingCommonUse"/>
+      <BaseTable v-model="building.use" :table-data="buildingData.getRareUse()" id="buildingRareUse"/>
+      <BaseTable v-model="building.details" :table-data="buildingData.getDetails()" id="buildingDetails"/>
+      <BaseTable v-model="building.items" :table-data="buildingData.getItems()" id="buildingItems"/>
     </template>
     <template v-slot:results>
-      <GeneratorResultElement :title="t('buildings.exterior.material.title')" :value="building.exteriorMaterial"/>
-      <GeneratorResultElement :title="t('buildings.exterior.details.title')" :value="building.exteriorDetails"/>
-      <GeneratorResultElement :title="t('buildings.use.title')" :value="building.use"/>
-      <GeneratorResultElement :title="t('buildings.details.title')" :value="building.details"/>
-      <GeneratorResultElement :title="t('buildings.items.title')" :value="building.items"/>
+      <GeneratorResultElement :title="t('buildings.exterior.material.title')" :value="building.exteriorMaterial" url="buildingExteriorMaterial"/>
+      <GeneratorResultElement :title="t('buildings.exterior.details.title')" :value="building.exteriorDetails" url="buildingExteriorDetails"/>
+      <GeneratorResultElement :title="t('buildings.use.title')" :value="building.use" url="buildingCommonUse"/>
+      <GeneratorResultElement :title="t('buildings.details.title')" :value="building.details" url="buildingDetails"/>
+      <GeneratorResultElement :title="t('buildings.items.title')" :value="building.items" url="buildingItems"/>
     </template>
   </BaseGenerator>
 </template>
